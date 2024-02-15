a4e7af19a1ef8655088faab8b54a3b13cd0a346d
Breelef <emil_vinther@hotmail.com>
2024-02-15 11:46:10 +0100

    .env

--- 08._Server-To-Server/node/app.js
+++ 08._Server-To-Server/node/app.js
@@ -1,6 +1,19 @@
 import express from "express";
 
+
+
 const app = express();
+
+
+app.get('/requestFastAPI', async (req, res) => {
+   const response = await fetch('http://127.0.0.1:8000/fastapidata');
+   const data = await response.json();
+   res.send(data);
+});
+
+app.get('/expressData', (req, res) => {
+   res.send({message: "isRunning" });
+});
 
 
 
